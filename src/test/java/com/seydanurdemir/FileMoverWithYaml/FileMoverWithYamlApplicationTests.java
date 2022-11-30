0c0f3fc2ac93b7d24b7d83b0e0c22f7f63c6919e
Seyda Nur DEMIR <dmrsydnr@gmail.com>
2022-11-30 15:36:51 +0300

    Added FileMoverWithYaml project

---
+++ src/test/java/com/seydanurdemir/FileMoverWithYaml/FileMoverWithYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seydanurdemir.FileMoverWithYaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileMoverWithYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
